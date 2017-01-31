4aa11d8e8622e82d9b64d0cc6915b8e6cf5aafe1
Michael E. Chancey Jr. <michael.chancey@gmail.com>
2017-01-31 11:22:02 -0500

    Formatted with Resharper

--- DelayedValidation.Tests/DelayedValidation.Test.cs
+++ DelayedValidation.Tests/DelayedValidation.Test.cs
@@ -37,6 +37,33 @@
 
         [TestMethod]
         [ExpectedException(typeof(ValidationException))]
+        public void DeSerialize_Invalid__Object()
+        {
+            var s =
+                JsonConvert.DeserializeObject<SomeDomainObject>(
+                    "{\"Age\":101,\"FirstName\":\"first\",\"IsDraft\":false,\"LastName\":\"last\"}");
+
+            Assert.AreEqual("first", s.FirstName);
+            Assert.AreEqual("last", s.LastName);
+            Assert.AreEqual(101, s.Age);
+            Assert.AreEqual(1, s.GetValidationErrors().Count);
+        }
+
+        [TestMethod]
+        public void DeSerialize_Invalid_Draftable_Object()
+        {
+            var s =
+                JsonConvert.DeserializeObject<SomeDomainObject>(
+                    "{\"Age\":101,\"FirstName\":\"first\",\"IsDraft\":true,\"LastName\":\"last\"}");
+
+            Assert.AreEqual("first", s.FirstName);
+            Assert.AreEqual("last", s.LastName);
+            Assert.AreEqual(101, s.Age);
+            Assert.AreEqual(1, s.GetValidationErrors().Count);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ValidationException))]
         public void Read_With_Invalid_Age_And_Draft_Mode_Off()
         {
             var s = new SomeDomainObject("first", "last", 101);
@@ -69,15 +96,6 @@
         }
 
         [TestMethod]
-        [ExpectedException(typeof(JsonSerializationException))]
-        public void Serialize_Invalid_Object()
-        {
-            var s = new SomeDomainObject("first", "last", 101);
-
-            var j = JsonConvert.SerializeObject(s);
-        }
-
-        [TestMethod]
         public void Serialize_Invalid_Draftable_Object()
         {
             var s = new SomeDomainObject("first", "last", 101);
@@ -88,30 +106,12 @@
         }
 
         [TestMethod]
-        public void DeSerialize_Invalid_Draftable_Object()
+        [ExpectedException(typeof(JsonSerializationException))]
+        public void Serialize_Invalid_Object()
         {
-            var s =
-                JsonConvert.DeserializeObject<SomeDomainObject>(
-                    "{\"Age\":101,\"FirstName\":\"first\",\"IsDraft\":true,\"LastName\":\"last\"}");
+            var s = new SomeDomainObject("first", "last", 101);
 
-            Assert.AreEqual("first", s.FirstName);
-            Assert.AreEqual("last", s.LastName);
-            Assert.AreEqual(101, s.Age);
-            Assert.AreEqual(1, s.GetValidationErrors().Count);
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(ValidationException))]
-        public void DeSerialize_Invalid__Object()
-        {
-            var s =
-                JsonConvert.DeserializeObject<SomeDomainObject>(
-                    "{\"Age\":101,\"FirstName\":\"first\",\"IsDraft\":false,\"LastName\":\"last\"}");
-
-            Assert.AreEqual("first", s.FirstName);
-            Assert.AreEqual("last", s.LastName);
-            Assert.AreEqual(101, s.Age);
-            Assert.AreEqual(1, s.GetValidationErrors().Count);
+            var j = JsonConvert.SerializeObject(s);
         }
 
         #endregion
